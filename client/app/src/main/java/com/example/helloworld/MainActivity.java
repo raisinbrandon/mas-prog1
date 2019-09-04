4aabe60c28dad5ec7772a01d531307f73b3a980d
Syed, Mohammed Sulaiman <msyed36@gatech.edu>
2019-09-03 22:37:44 -0400

    added the scroll feature for textView. Displays everything although only once in the textView when send button gets clicked.

--- client/app/src/main/java/com/example/helloworld/MainActivity.java
+++ client/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText messageEntryBox = (EditText) findViewById(R.id.messageEntryBox);
+                TextView messageList = (TextView) findViewById(R.id.messageList);
+
+                String text = messageEntryBox.getText().toString();
+                messageList.setTextSize(TypedValue.COMPLEX_UNIT_SP, 27);
+                messageList.setText(text);
+            }
+        });
     }
 }
